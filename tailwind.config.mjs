5b2883a8891761ef928466551357af05d1e5ec6e
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-02-07 21:58:09 -0500

    1er commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+export default {
+    content: [
+        './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
